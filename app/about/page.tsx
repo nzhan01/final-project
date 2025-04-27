201d3c6dea6a971b54849e4170400007a27d1a4a
Nicholas Zhang <nzhan01@bu.edu>
2025-04-27 16:56:58 -0400

    added info to about page and header componenet

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function AboutPage() {
+
+
+    return(
+
+        <div className="flex flex-col justify-center items-center bg-red-300 p-2 ">
+
+            <h1> This is our CS391 final project</h1>
+            <p> We made a cocktail generator and search app</p>
+        </div>
+    );
+}
