19c4035e38438ad81eac911f33c1dc0d7f952dfd
annika-gupt <annigupt@bu.edu>
2025-04-27 16:52:33 -0400

    Update types.ts

---
+++ types.ts
@@ -0,0 +1,11 @@
+export type CocktailType = {
+    idDrink: string;
+    strDrink: string;
+    strCategory: string;
+    strAlcoholic: string;
+    strGlass: string;
+    strInstructions: string;
+    strInstructionsES: string;
+    strInstructionsFR: string;
+    strDrinkThumb: string;
+}
