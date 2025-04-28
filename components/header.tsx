f17dbb03f7b06681f364263510cb1c8e6d0c86ef
Nicholas Zhang <nzhan01@bu.edu>
2025-04-28 18:17:58 -0400

    finished header with navbar and added it to layout.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,10 @@ export default function Header() {
     const linkStyling = " p-1 m-2 text-xl hover:underline"
     return(
         <header className="flex justify-between items-center  h-20">
-            <h2 className = "text-4xl font-semibold p-4">MP5 Api App</h2>
+            <h2 className = "text-4xl font-semibold p-4">CS391 Final Project Cocktail App</h2>
             <nav className = "p-2 m-4">
-                <Link href="/" className = {linkStyling}>
-                    Home
+                <Link href="/random" className = {linkStyling}>
+                    random
                 </Link>
                 <Link href="/about" className = {linkStyling}>
                     About
