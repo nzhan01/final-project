ed74256b84f238f451969bdb068e09d2aec83e25
Nicholas Zhang <nzhan01@bu.edu>
2025-04-28 18:56:10 -0400

    styled the header

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,36 @@
-
+"use client"
 import Link from "next/link";
+//background-image: linear-gradient(to top,  #D62828, #F77F00,#FCBF49,#EAE2B7
 
+
+
+import styled from "styled-components";
+
+//https://stackoverflow.com/questions/8384751/css-text-gradient
+const StyledTitle = styled.h2`
+    
+    background-image: linear-gradient(to top,  #D62828, #F77F00,#FCBF49);
+    color: transparent;
+    background-clip: text;
+    padding: 0.5vh;
+    font-size: calc(15px + 2vh);
+    font-weight: bold;
+    
+`
 export default function Header() {
-    const linkStyling = " p-1 m-2 text-xl hover:underline"
+    const linkStyling = " p-2 m-5 text-2xl hover:underline"
     return(
-        <header className="flex justify-between items-center  h-20">
-            <h2 className = "text-4xl font-semibold p-4">CS391 Final Project Cocktail App</h2>
-            <nav className = "p-2 m-4">
+        <header className="flex justify-between items-center h-20 p-2">
+            <Link href ="/">
+                <StyledTitle>CS391 Final Project Cocktail App</StyledTitle>
+            </Link>
+
+            <nav>
+                <Link href ="/" className ={linkStyling}>
+                    Search
+                </Link>
                 <Link href="/random" className = {linkStyling}>
-                    random
+                    Random
                 </Link>
                 <Link href="/about" className = {linkStyling}>
                     About
