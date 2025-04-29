3ee8840ff95f670a460f889e88fbecdde91a18c2
Chih-Hsuan Lin <hsuan712@bu.edu>
2025-04-29 12:33:52 -0400

    searchrandom comments

--- app/random/SearchRandom.tsx
+++ app/random/SearchRandom.tsx
@@ -1,10 +1,17 @@
+// Author: Chih-Hsuan (Cindy) Lin
+// SearchRandom.tsx: Fetches and displays a random cocktail from the CocktailDB API with an option to get a new drink.
+
+// Enable client-side rendering
 "use client";
+
+// Import the Cocktail display component and the CocktailType definition
 import Cocktail from "../../components/Cocktail";
 import {CocktailType} from "@/types";
 
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+// Styled components
 const Card = styled.div`
   background-color: #FAD59A;
   border-radius: 12px;
@@ -32,8 +39,10 @@ const StyledButton = styled.button`
 `;
 
 export default function DrinkGenerator() {
+    // State to store the randomly fetched cocktail
     const [cocktail, setCocktail] = useState<CocktailType | null>(null);
 
+    // Fetch a random cocktail from CocktailDB API
     async function fetchRandomCocktail() {
         try {
             const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
@@ -44,6 +53,7 @@ export default function DrinkGenerator() {
         }
     }
 
+    // Fetch a drink
     useEffect(() => {
         fetchRandomCocktail();
     }, []);
--- app/random/page.tsx
+++ app/random/page.tsx
@@ -1,3 +1,5 @@
+// Author: Chih-Hsuan (Cindy) Lin
+// RandomPage.tsx -- Renders the DrinkGenerator component, which fetches and displays a random cocktail.
 import DrinkGenerator from "./SearchRandom";
 
 export default function RandomPage() {
