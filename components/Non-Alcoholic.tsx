5e0595afc6ea00bd330422a5658cd79288020de6
Nicholas Zhang <nzhan01@bu.edu>
2025-04-29 11:23:22 -0400

    styled the drinks on non-alcholic.tsx

--- components/Non-Alcoholic.tsx
+++ components/Non-Alcoholic.tsx
@@ -2,11 +2,58 @@
 import {useEffect, useState} from "react";
 import styled from "styled-components";
 
+
 const StyledContainer = styled.div`
 
     display: flex;
     flex-direction: column;
     align-items: center;
+    font-family: "EB Garamond";
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 40px 20px;
+`
+const StyledImage = styled.img`
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    border-radius: 12px;
+    flex-shrink: 0;
+`
+
+const StyledDrinkCard = styled.div`
+    background-color: #f9d89c;
+    padding: 20px;
+    border-radius: 12px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    transition: transform 0.2s ease;
+    width: 40%;
+`
+
+const StyledTitle = styled.h1`
+    text-align: center;
+    font-size: 2.5rem;
+    color: #5c3d00;
+    margin-bottom: 30px;`
+
+const StyledDrinkTitle = styled.h3`
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #5c3d00;`
+
+const StyledDrinkList = styled.div`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+    align-items: center;
+
 `
 
 type cocktail = {
@@ -14,6 +61,8 @@ type cocktail = {
     strDrink: string;
     strDrinkThumb: string;
 };
+
+
 
 export default function NonAlcoholic() {
     const[data, setData] = useState<cocktail[]>([]);
@@ -36,14 +85,22 @@ export default function NonAlcoholic() {
 
     return (
         <StyledContainer>
+            <StyledTitle>Non-Alcoholic Drinks</StyledTitle>
+            <StyledDrinkList>
+                {data.slice(0,20).map((drink) => (
+                    <StyledDrinkCard key={drink.idDrink}>
+                        <StyledImage src={drink.strDrinkThumb} alt={drink.strDrink} width="100" />
 
-            {data.slice(0,20).map((drink) => (
-                <div key={drink.idDrink}>
-                    <h3>{drink.strDrink}</h3>
-                    <img src={drink.strDrinkThumb} alt={drink.strDrink} width="100" />
-                </div>
-            ))}
+                        <StyledDrinkTitle>{drink.strDrink}</StyledDrinkTitle>
+
+                    </StyledDrinkCard>
+
+                ))}
+            </StyledDrinkList>
+
         </StyledContainer>
 
     )
+
+
 }
